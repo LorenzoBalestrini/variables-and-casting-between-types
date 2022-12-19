093e2ca9f2deed23a7b6f67101b0a4a7d2163987
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-19 14:54:33 +0100

    variables and casting between types

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val number1 : Int = 5
+    val number2 : Double = 7.8
+    val number3 : Float = 3.5f
+    val result = number1 + number2 + number3
+    println("Result : $result")
+}
